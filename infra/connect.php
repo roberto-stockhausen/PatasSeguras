c6b9a7a38648abc42b211c7e90423cdb585a4784
Roberto Hille <roberto_hille@estudante.sesisenai.org.br>
2026-08-24 11:34:47 -0300

    add - connect script

--- infra/connect.php
+++ infra/connect.php
@@ -1,5 +1,15 @@
 <?php
 
+$host = "localhost";
+$user = "root";
+$password = "root";
+$database = "roberto_patas_seguras";
 
+$conn = new mysqli($host, $user, $password, $database);
+
+if ($conn->connect_error)
+    {
+        die("Não foi possível conectar com o servidor: " . $conn->connect_error);
+    }
 
 ?>
